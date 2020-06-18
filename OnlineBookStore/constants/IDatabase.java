f39a077f73543cb739eebe48477a48b104b80c3a
Krati <Krati@LAPTOP-NQLJL8JJ>
2020-06-18 15:51:53 +0530

    First Commit on My repo

--- OnlineBookStore/constants/IDatabase.java
+++ OnlineBookStore/constants/IDatabase.java
@@ -5,6 +5,6 @@ public class IDatabase {
 	public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
 	public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/onlinebookstore";
 	public static final String USER_NAME = "newuser";
-	public static final String PASSWORD = "Temp@123";
+	public static final String PASSWORD = "Secret@123";
 
 }
